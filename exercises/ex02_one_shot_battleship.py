4ecd12f5fff70eab23a15a46b56be2cf03c700e0
Ella Nichols <enichols1@unc.edu>
2024-02-01 08:50:10 -0500

    am changes

--- exercises/ex02_one_shot_battleship.py
+++ exercises/ex02_one_shot_battleship.py
@@ -7,16 +7,16 @@ correct: bool = False
 while not correct:
     guess_row: str = input("Guess a row: ")
     row_int: int = int(guess_row)
-    if row_int > 4:
-        row_int:int(input(f"The grid is only {GRID_SIZE} by {GRID_SIZE}. Try again: "))
+    if row_int != range(1, GRID_SIZE):
+        row_int:int = int(input(f"The grid is only {GRID_SIZE} by {GRID_SIZE}. Try again: "))
     if row_int != SECRET_ROW:
         print("Miss!")
     guess_column: str = input("Guess a column: ")
     column_int: int = int(guess_column)
-    if column_int > 4:
-        column_int:int(input(f"The grid is only {GRID_SIZE} by {GRID_SIZE}. Try again: "))
+    if column_int != range(1,GRID_SIZE):
+        column_int: int = int(input(f"The grid is only {GRID_SIZE} by {GRID_SIZE}. Try again: "))
     if column_int != SECRET_COLUMN:
         print("Miss!")
     correct = True
-
-
+else:
+    print("Hit!")
