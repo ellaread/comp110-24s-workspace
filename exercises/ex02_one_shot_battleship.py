64a169f7dd0fc9544e997d72f4d89e10e50e7f79
Ella Nichols <enichols1@unc.edu>
2024-02-01 11:56:31 -0500

    part 1 complete

--- exercises/ex02_one_shot_battleship.py
+++ exercises/ex02_one_shot_battleship.py
@@ -3,17 +3,22 @@ __author__ = "730487065"
 SECRET_ROW: int = 3
 SECRET_COLUMN: int = 2
 GRID_SIZE: int = 4
+BLUE_BOX: str = "\U0001F7E6"
+RED_BOX: str = "\U0001F7E5"
+WHITE_BOX: str = "\U00002B1C"
+
 correct: bool = False
 while not correct:
     guess_row: str = input("Guess a row: ")
     row_int: int = int(guess_row)
-    if row_int != range(1, GRID_SIZE):
-        row_int:int = int(input(f"The grid is only {GRID_SIZE} by {GRID_SIZE}. Try again: "))
+    if row_int > GRID_SIZE or row_int < 1:
+        row_int: int = int(input(f"The grid is only {GRID_SIZE} by {GRID_SIZE}. Try again: "))
+        # i think i need another conditional in here so this prompt keeps repeating until the value is in range
     if row_int != SECRET_ROW:
         print("Miss!")
     guess_column: str = input("Guess a column: ")
     column_int: int = int(guess_column)
-    if column_int != range(1,GRID_SIZE):
+    if column_int > GRID_SIZE or column_int < 1: 
         column_int: int = int(input(f"The grid is only {GRID_SIZE} by {GRID_SIZE}. Try again: "))
     if column_int != SECRET_COLUMN:
         print("Miss!")
