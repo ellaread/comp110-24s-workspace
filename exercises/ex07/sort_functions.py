b128598bacb87280c623a9070ce9a0223703ab36
Ella Nichols <enichols1@unc.edu>
2024-03-25 11:57:52 -0400

    insertion functions

--- exercises/ex07/sort_functions.py
+++ exercises/ex07/sort_functions.py
@@ -3,13 +3,26 @@
 __author__ = ""
 
 def insertion_sort(x: list[int]) -> None:
-    """Basic insertion sort algorithm.
-    Insert into an already sorted list."""
+    """Basic insertion sort algorithm. Insert into an already sorted list."""
+
     return None
 
 
 def selection_sort(x: list[int]) -> None:
-    """Basic selection sort algorithm. 
-    Repeatedly find the minimum and swap it."""
+    """Basic selection sort algorithm. Repeatedly find the minimum and swap it."""
+    x = [7, 3, 8, 5, 2, 4]
+    counter = 0
+    while counter < len(x):
+        min_idx = counter
+        secondary_counter = counter + 1 
+        while secondary_counter < len(x): 
+            if x[secondary_counter] < x[min_idx]:
+                min_idx = secondary_counter
+            secondary_counter += 1 
+        if min_idx != counter: 
+            temporary_int = x[counter]
+            x[counter] = x[min_idx]
+            x[min_idx] = temporary_int
+        counter =+ 1 
     return None
     
